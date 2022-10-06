68d9923a2402f8cf25c451b77816ef0bd253face
PleasantPlant <70914450+PleasantPlant@users.noreply.github.com>
2022-10-06 12:34:28 -0400

    Update equipment.py

--- equipment.py
+++ equipment.py
@@ -1,10 +1,18 @@
 
 
-class Armor():
-    def __init__(self) -> None:
-        self.name = "Armor"
-        self.cost = 0
+class Armor:
+    def __init__(self):
+        self.name = "No Armor"
+        self.price = 0
+        self.weight = 0
+        self.ac = 11
 
 
-class Weapon():
-    pass
+class Weapon:
+    def __init__(self):
+        self.name = "No Weapon"
+        self.price = 0
+        self.weight = 0
+        self.size = 0
+        self.l_d = 0
+        self.h_d = 0
