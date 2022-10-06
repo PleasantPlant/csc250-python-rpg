4591cd3e2033b7cd736ac753078162f393b29b23
PleasantPlant <70914450+PleasantPlant@users.noreply.github.com>
2022-10-06 12:33:36 -0400

    Update combat.py

--- combat.py
+++ combat.py
@@ -1,6 +1,38 @@
 
 
 class Combat():
+   class Combat:
     def __init__(self):
-        ##init should load a list of combatants and assign initiatives to each of them.
+        self.player_characters = "character.py"
+        self.npc = "monster.py"
+        self.interactive_mode = True
+        self.party_xp = 0
+        self.party_success = False
+        self.ordered_combatants = 0
+        self.player_ply_function = ""
+        self.endgame_function = ""
+
+    def characters_dead(self):
+        self.players_dead = False
+        self.npc_dead = False
+
+    def combat_over(self):
+        self.players_dead = True
+        self.npc_dead = True
+
+    def end_combat(self):
+        self.endgame_function
+
+    def ply(self):
+        self.player_ply = True
+        self.npc_ply = True
+
+    def print_stats(self):
+        self.stats = ""
+
+    def turn(self):
+        self.player_turn = ""
+        self.npc_turn = ""
+
+    def start(self):
         pass
